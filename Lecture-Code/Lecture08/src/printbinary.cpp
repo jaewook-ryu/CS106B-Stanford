af2028543a877a9e4bafd72ac5f6aaa1be098b83
Jaewook Ryu <jr3576@columbia.edu>
2021-04-11 00:40:44 +0900

    Added printDecimal

--- Lecture-Code/Lecture08/src/printbinary.cpp
+++ Lecture-Code/Lecture08/src/printbinary.cpp
@@ -5,6 +5,69 @@
 #include "vector.h"  // for Vector
 using namespace std;
 
-int main() {
+void printAllBinaryVer01(int digit);
+void printAllBinaryVer02(int digit);
+void binaryDigitHelper(int digit, string output);
 
+void printDecimal(int digit);
+void printDecimalHelper(int digit, string output);
+
+int main() {
+    int digit;
+    cout << "Enter number of digits: ";
+    cin >> digit;
+
+    //printAllBinaryVer02(digit);
+
+
+    return 0;
 }
+
+void printDecimal(int digit){
+    printDecimalHelper(digit, "");
+}
+
+void printDecimalHelper(int digit, string output){
+    if(digit == 0){
+        cout << output << endl;
+    } else{
+        for(int i=0;i<9;i++){
+            printDecimalHelper(digit-1, output + integerToString(i));
+        }
+    }
+}
+
+
+
+void printAllBinaryVer01(int digit){
+    // base case
+    if(digit == 1){
+        cout << 0 << endl;
+        cout << 1 << endl;
+    }
+
+    // non-base cases
+    else{
+        cout << 0;
+        printAllBinaryVer01(digit-1);
+
+        cout << 1;
+        printAllBinaryVer02(digit-1);
+    }
+}
+
+void printAllBinaryVer02(int digit){
+    binaryDigitHelper(digit, "");
+}
+
+// helper function
+void binaryDigitHelper(int digit, string output){
+    if(digit == 0){
+        cout << output << endl;
+    } else{
+        binaryDigitHelper(digit-1, output + "0");
+
+        binaryDigitHelper(digit-1, output + "1");
+    }
+}
+
