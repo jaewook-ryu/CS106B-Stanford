6037ba2c064aa5f9aa0f41f02c89132390aa8660
= <=>
2021-04-13 19:36:39 +0900

    Updated escapeMaze.cpp

--- Lecture-Code/Lecture09/simple-project/src/mazesolver.cpp
+++ Lecture-Code/Lecture09/simple-project/src/mazesolver.cpp
@@ -4,5 +4,29 @@
 
 bool escapeMaze(Maze& maze, int row, int col){
     // base case
-    if(!maze.inBounds(row, col))
+    if(!maze.inBounds(row, col)){
+        return true;
+    } else if(maze.isWall(row, col)){
+        return false;
+    } else if(!maze.isOpen(row, col)){
+        // choose step
+        maze.mark(row, col);
+
+        // exploring steps
+        bool result = escapeMaze(maze, row-1, col) || escapeMaze(maze, row+1, col) ||
+                escapeMaze(maze, row, col-1) || escapeMaze(maze, row, col+1);
+
+        // un-choose if it is not a good result
+        if(!result){
+            maze.taint(row, col);
+        }
+
+        return result;
+
+    } else{
+        return false;
+    }
+
+
+
 }
