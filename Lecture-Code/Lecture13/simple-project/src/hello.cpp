c376313672bf1a8249d66cb1f58b514ff27d7c1d
Jaewook Ryu <jr3576@columbia.edu>
2021-04-24 19:36:25 +0900

    Saving during lecture

--- Lecture-Code/Lecture13/simple-project/src/hello.cpp
+++ Lecture-Code/Lecture13/simple-project/src/hello.cpp
@@ -1,56 +1,83 @@
 #include <iostream>
 #include "console.h"
-#include "gwindow.h" // for GWindow
-#include "simpio.h"  // for getLine
-#include "vector.h"  // for Vector
 using namespace std;
 
-int main() {
-    cout << "This sample project helps you check and make sure that" << endl;
-    cout << "you have successfully install Qt Creator, C++, and Java," << endl;
-    cout << "and that the Stanford C++ libraries work on your machine." << endl;
-    cout << endl;
-    cout << "If this project is working properly on your computer," << endl;
-    cout << "You should see a \"Sample Project\" window with a" << endl;
-    cout << "smiley face pop up when you build and run it." << endl;
-    cout << endl;
-    cout << "If it doesn't run, check that you have installed" << endl;
-    cout << "Java JDK properly, as well as C++ and Qt Creator." << endl;
-    cout << endl;
 
-    GWindow window(300, 200);
-    window.setColor("White");
-    window.clear();
-    window.setWindowTitle("Sample Project");
-    window.setColor("Black");
-    window.drawOval( 10, 10, 180, 180);   // head
-    window.fillOval( 50, 50,  20,  20);   // eye
-    window.fillOval(130, 50,  20,  20);   // eye
-    window.fillOval( 95, 95,  10,  10);   // nose
+struct ListNode{
+    int data;
+    ListNode* next;
+};
 
-    // draw the mouth using a Vector to make sure the Stanford collections work
-    Vector<GPoint> mouth;
-    mouth.add(GPoint( 40, 120));
-    mouth.add(GPoint( 70, 150));
-    mouth.add(GPoint(130, 150));
-    mouth.add(GPoint(160, 120));
-    for (int i = 0; i < mouth.size() - 1; i++) {
-        GPoint p1 = mouth.get(i);
-        GPoint p2 = mouth.get(i + 1);
-        window.drawLine(p1, p2);   // mouth
+/* Incorrect way: duplicate of pointer created.
+void addFront(ListNode* front, int value){
+
+    ListNode* temp = new ListNode(value);
+
+    temp->text = front;
+    front = temp;
+}
+*/
+
+void addFront(ListNode*& list, int value){
+    // instantiating a new struct.
+    ListNode* temp = new ListNode();
+
+    // set value for the node.
+    temp->data = value;
+
+    // the temp node will point to the first node of the previous list.
+    temp->next = list;
+
+    // we are modifying the pointer to point at the first node(temp).
+    list = temp;
+}
+
+void addBack(ListNode*& front, int value){
+    ListNode* current = front;
+
+    // check for null pointer
+    if(front == nullptr){
+        front = new ListNode();
+        front->data = value;
+    } else{
+        // this for loop points at the last node of the list
+        for(;current->next != nullptr;current = current->next);
+
+        // insantiate new node that will be the last node.
+        ListNode* temp = new ListNode();
+        temp->data = value;
+        temp->next = nullptr;
+
+        // set the last node to be the temp ListNode;
+        current->next = temp;
     }
 
-    window.setVisible(true);
+    /* The previous expression is equivalent to
+    while (list->next != nullptr){
+        list = list->next;
+    }
+    */
 
-    window.requestFocus();
-    pause(3000);
 
-    getLine("Press [Enter] to close the window... ");
-    window.requestFocus();
-    window.close();
-    exitGraphics();
+}
 
-    cout << endl << "Program completed." << endl;
+void printList(ListNode* front){
+    while(front != nullptr){
+        cout << front->data << endl;
+        front = front->next;
+    }
+}
+
+
+int main() {
+    ListNode* front = nullptr;
+    //addFront(front, 9);
+    //addFront(front, 17);
+    //addFront(front, -3);
+    //addFront(front, 42);
+
+    addBack(front, 20);
+    printList(front);
 
     return 0;
 }
