1385271c0ee799cd564bd5b640b5fdd35c94b694
Jaewook Ryu <jr3576@columbia.edu>
2021-04-29 20:20:29 +0900

    finished lecture15

--- Lecture-Code/Lecture15/TreeNode.cpp
+++ Lecture-Code/Lecture15/TreeNode.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+using namespace std;
+
 struct TreeNode{
     int data;
     TreeNode *left;
     TreeNode *right;
+
+    TreeNode(int data){
+        this->data = data;
+    }
 
     TreeNode(int data, TreeNode* left, TreeNode* right){
         this->data = data;
@@ -13,3 +20,37 @@ struct TreeNode{
         return (left == nullptr) && (right == nullptr);
     }
 };
+
+void printTree(TreeNode* node){
+    if(node->right == nullptr && node->left == nullptr){
+        cout << node->data << endl;
+
+    } else{
+        printTree(node->right);
+        printTree(node->left);
+    }
+
+    /*
+    if(node != nullptr){
+        cout << node->data << endl;
+        printTree(node->right);
+        printTree(node->left);
+    }
+    */
+}
+
+int sizeTree(TreeNode* node){
+    if(node == nullptr){
+        return 0;
+    }
+
+    return 1 + sizeTree(node->right) + sizeTree(node->left);
+}
+
+
+int main(){
+    TreeNode* root = new TreeNode(2);
+    printTree(root);
+
+    return 0;
+}
